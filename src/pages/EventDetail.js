e5ace139e37484a4df6863bc2a12cc7acabace98
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-03-26 20:30:47 +0800

    Submitting Data Programmatically

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouteLoaderData,json } from 'react-router-dom';
+import { useRouteLoaderData,json, redirect } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 
 const EventDetailPage = () => { 
@@ -21,4 +21,19 @@ export async function loader({request,params}){
   }else{
     return response
   }
+}
+export async function action(params,request){
+  const eventId=params.params.eventId;
+  console.log(eventId)
+  console.log(request)
+  // const response=await fetch('http://localhost:8080/events/'+eventId,{
+  //   method:'DELETE',
+  // });
+  // if(!response.ok){
+  //   throw json(
+  //     {message:'Could not delete event'},
+  //     {status:500}
+  //     )
+  // }
+  // return redirect('/events')
 }
